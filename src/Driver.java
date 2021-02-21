783f3ab6621c0be777e8afd2b9a0967c2b628bf6
HappyJohn573 <78828034+HappyJohn573@users.noreply.github.com>
2021-02-21 15:39:03 -0600

    Updatee for HW #7

--- src/Driver.java
+++ src/Driver.java
@@ -9,6 +9,13 @@ public class Driver
 
 	public static void main(String[] args)
 	{ 
+		HearthstoneCard c1 = new HearthstoneCard("Mana Wyrm", 2, 1, 3);
+		HearthstoneCard c2 = new HearthstoneCard("Sylvanias Windrunner", 6, 5, 5);
+		HearthstoneCard c3 = new HearthstoneCard("Savannah Highmane", 6, 6, 5);
+		c1.display();
+		c2.display();
+		c3.display();
+		
 		URLReader hearthstoneURLReader = new URLReader("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
 		Object obj = JSONValue.parse(hearthstoneURLReader.getTheURLContents());
 		HearthstoneCard[] theMinions = new HearthstoneCard[1924];
